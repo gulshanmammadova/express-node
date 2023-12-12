ba4c6da3c5c4dfa12638523827366a3ab552404b
gulshanmammadova <gulshanfm@code.edu.az>
2023-12-12 09:34:29 -0800

    Node-Expres-task1

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const fs = require('fs');
+
+fs.readFile('./data.json', 'utf8', (err, data) => {
+  if (!err) {
+    app.get('/', function (req, res) {
+        res.send(data);
+      });
+  } else {
+    console.error(err);
+  }
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
